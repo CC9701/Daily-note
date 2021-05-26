778d24b24c104920682a61cb21e82f80066dc682
CC9701 <73738313+CC9701@users.noreply.github.com>
2021-05-27 00:27:22 +0800

    Add files via upload

---
+++ 图形相关/WebGL/WebGL Programing Guide/Code/Chapter4/Listing4.4 ControlRotatedTriangle/ControlRotatedTriangle.js
@@ -0,0 +1,132 @@
+// RotatedTriangle_Matrix4.js (c) 2012 matsuda
+// Vertex shader program
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'uniform mat4 u_ModelMatrix;\n' +
+    'void main() {\n' +
+    '  gl_Position = u_ModelMatrix * a_Position;\n' +
+    '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE =
+    'void main() {\n' +
+    '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+    '}\n';
+
+var ANGLE_STEP = 45.0;
+function main() {
+    // Retrieve <canvas> element
+    var canvas = document.getElementById('webgl');
+
+    // Get the rendering context for WebGL
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    // Write the positions of vertices to a vertex shader
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+     // Pass the rotation matrix to the vertex shader
+    var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+    if (!u_ModelMatrix) {
+        console.log('Failed to get the storage location of u_ModelMatrix');
+        return;
+    }
+
+
+    // Current rotation angle
+    var currentAngle = 0.0;
+    // Model matrix
+    var modelMatrix = new Matrix4();
+
+    // Start drawing
+    var tick = function() {
+        currentAngle = animate(currentAngle);  // Update the rotation angle
+        draw(gl, n, currentAngle, modelMatrix, u_ModelMatrix);   // Draw the triangle
+        requestAnimationFrame(tick, canvas); // Request that the browser calls tick
+    };
+    tick();
+}
+
+
+var tick = function(){
+    currentAngle = animation(currentAngle);
+    draw(gl, n, currentAngle, modelMatrix, u_Modelmatrix);
+    requestAnimationFrame(tick);
+}
+var g_last = Date.now(); 
+function animate(angle) {
+    // 计算与上一次调用now函数的时间差（大致等于画这一帧画面与前一帧画面的时间差）
+    var now = Date.now();
+    var elapsed = now - g_last;
+    g_last = now;
+    // 更新旋转角度
+    var newAngle = angle + (ANGLE_STEP * elapsed) / 1000.0;
+    return newAngle %= 360;
+
+}
+
+function draw(gl, n, currentAngle, modelMatrix, u_ModelMatrix) {
+    // Set the rotation matrix
+    modelMatrix.setRotate(currentAngle, 0, 0, 1);
+    // Pass the rotation matrix to the vertex shader
+    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // Draw the traingle
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        0, 0.5, -0.5, -0.5, 0.5, -0.5
+    ]);
+    var n = 3; // The number of vertices
+
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return false;
+    }
+
+    // Bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // Write date into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return -1;
+    }
+    // Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    // Enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
+
+function up() {
+    ANGLE_STEP += 10; 
+  }
+  
+  function down() {
+    ANGLE_STEP -= 10; 
+  }
+  
+
